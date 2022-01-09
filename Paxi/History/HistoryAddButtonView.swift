2a2252b74f2d7b8ef5b8bd6fb51a1a6e81fc7741
Eric <eric.morales@protonmail.com>
2022-01-09 18:43:45 -0400

    feat(history): add plusButton and searchButtonBarItem

---
+++ Paxi/History/HistoryAddButtonView.swift
@@ -0,0 +1,35 @@
+//
+//  HistoryAddButtonView.swift
+//  Paxi
+//
+//  Created by Eric Morales on 1/9/22.
+//
+
+import UIKit
+
+class HistoryAddButtonView: UIButton {
+    
+    // MARK: Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(buttonSize: CGSize, vcTintColor: UIColor) {
+        self.init(frame: CGRect(x: 0, y: 0, width: buttonSize.width, height: buttonSize.height))
+        
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.2
+        layer.cornerRadius = buttonSize.width / 2
+        backgroundColor = .secondarySystemBackground
+        tintColor = vcTintColor
+        
+        let image = UIImage(systemName: "plus",
+                            withConfiguration: UIImage.SymbolConfiguration(pointSize: 32,
+                                                                           weight: .medium))
+        setImage(image, for: .normal)
+    }
+}
--- Paxi/History/HistoryVC.swift
+++ Paxi/History/HistoryVC.swift
@@ -9,21 +9,60 @@ import UIKit
 
 class HistoryVC: UIViewController {
     
+    // MARK: Properties
+    lazy var vcTintColor: UIColor = .systemOrange
+    lazy var buttonSize: CGSize = CGSize(width: 60, height: 60)
+    lazy var addButton: UIButton = HistoryAddButtonView(buttonSize: buttonSize, vcTintColor: vcTintColor)
+    
+    // MARK: VC's Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+        
+        setupPlusButton()
+        setupNavigationController()
     }
     
+    // MARK: Methods
+    func setupNavigationController() {
+        // add search button
+        let searchButtonBarItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"),
+                        style: .plain, target: self, action: #selector(searchButtonSelected))
+        
+        self.navigationItem.rightBarButtonItem = searchButtonBarItem
+        
+        // style title
+        self.navigationController?.navigationBar.prefersLargeTitles = false
+        let titleTextAttribute: [NSAttributedString.Key : Any] = [ .foregroundColor: UIColor.clear,]
+        self.navigationController?.navigationBar.titleTextAttributes = titleTextAttribute
+        
+        // add color
+        self.navigationController?.navigationBar.tintColor = vcTintColor
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    func setupPlusButton() {
+        self.view.addSubview(addButton)
+        
+        let buffer: CGFloat = 20
+        addButton.frame = CGRect(x: self.view.frame.size.width - buttonSize.width - buffer,
+                                 y: self.view.frame.size.height - buttonSize.height - buffer,
+                                 width: buttonSize.width, height: buttonSize.height)
+        
+        addButton.addTarget(self, action: #selector(addButtonSelected), for: .touchUpInside)
+    }
     
+    @objc func addButtonSelected() {
+        let alert = UIAlertController(title: "Add Something", message: "Add button has been tapped.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        
+        present(alert, animated: true)
+    }
+    
+    @objc func searchButtonSelected() {
+        let alert = UIAlertController(title: "Search Something", message: "Search button has been tapped.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        
+        present(alert, animated: true)
+    }
 }
